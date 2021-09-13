7fee0f194444b23959e05b3f4c5ed331fe646b64
sanjaymanikandan <sanjumani619@gmail.com>
2021-09-13 18:45:16 +0530

    First commit

---
+++ crud_project/crud_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=40)
+    adress=models.CharField(max_length=50)
+    jobrole=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
